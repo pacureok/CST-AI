762a8a85e864e1fa092520560e7fc6da60b9dc8c
pacureok <okkpacur@gmail.com>
2026-02-01 19:46:50 +0000

    Fix: Added __version__ to triton mock to satisfy PyTorch Inductor check

--- MODELS/triton/__init__.py
+++ MODELS/triton/__init__.py
@@ -1,5 +1,13 @@
+# CST-Mock: Identidad de versión para engañar a PyTorch Inductor
+__version__ = '3.0.0' 
+
 from . import language
+from . import compiler
+from . import runtime
+
 class Config:
     def __init__(self, *args, **kwargs): pass
+
 def jit(x): return x
+
 HAS_WARP_SPEC = False
