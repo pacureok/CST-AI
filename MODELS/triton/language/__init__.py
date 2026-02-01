0955affb5d13961581b14c5d2f45875f92af0a73
pacureok <okkpacur@gmail.com>
2026-02-01 19:48:48 +0000

    Fix: Added constexpr to triton.language mock for PyTorch math compatibility

--- MODELS/triton/language/__init__.py
+++ MODELS/triton/language/__init__.py
@@ -1,13 +1,20 @@
-from . import core
-from . import extra
 import sys
 
-# Definimos los mocks para que sean accesibles como atributos
-class MathMock:
-    pass
+# Definimos constexpr como una clase que simplemente devuelve lo que recibe
+class constexpr:
+    def __init__(self, value):
+        self.value = value
+    def __repr__(self):
+        return str(self.value)
 
-math = MathMock()
+# Añadimos constantes matemáticas básicas que Inductor suele buscar
+def log2(x): return x # Mock simple
+def exp(x): return x
 
-# Inyectamos los módulos en el registro global de Python
+# Aseguramos que 'core' y 'extra' sigan vinculados
+from . import core
+from . import extra
+
+# Inyectamos los submódulos en el registro de Python para evitar AttributeError
 sys.modules['triton.language.core'] = core
 sys.modules['triton.language.extra'] = extra
